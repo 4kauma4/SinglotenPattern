368d3da4242c2cc3342359f37936b7fb00f65b02
Dobby <159747473+4kauma4@users.noreply.github.com>
2024-05-15 11:24:11 +0700

    Add files via upload

---
+++ SinglotenPattern/Servers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        ServersSingl servers = ServersSingl.Instance;
+
+
+        Console.WriteLine(servers.AddServer("http://example.com"));//true
+        Console.WriteLine(servers.AddServer("https://example.com"));// true
+        Console.WriteLine(servers.AddServer("ftp://example.com"));// false
+        Console.WriteLine(servers.AddServer("https://example.com"));// дупликат - false
+
+
+        Console.WriteLine("HTTP servers:");
+        foreach (var server in servers.GetHttpServers())
+        {
+            Console.WriteLine(server);
+        }
+        
+        Console.WriteLine("HTTPS servers:");
+        foreach (var server in servers.GetHttpsServers())
+        {
+            Console.WriteLine(server);
+        }
+    }
+}
---
+++ SinglotenPattern/ServrsLibrary/ServersSingl.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ServersSingl
+{
+    private HashSet<string> servers = new HashSet<string>();
+    private static readonly Lazy<ServersSingl> lazy = new Lazy<ServersSingl>(() => new ServersSingl());
+
+    public static ServersSingl Instance => lazy.Value;
+
+    private ServersSingl() { } 
+
+
+    public bool AddServer(string serverUrl) // проверка на http or https
+    {
+
+        if (string.IsNullOrWhiteSpace(serverUrl) ||
+            (!serverUrl.StartsWith("http://") && !serverUrl.StartsWith("https://")))
+        {
+            return false;
+        }
+
+
+        return servers.Add(serverUrl); // возврат bool pyfxtybz
+    }
+
+
+    public List<string> GetHttpServers() // url серверов с http
+    {
+        return servers.Where(url => url.StartsWith("http://")).ToList();
+    }
+
+
+    public List<string> GetHttpsServers() // url серверов с https
+    {
+        return servers.Where(url => url.StartsWith("https://")).ToList();
+    }
+}
